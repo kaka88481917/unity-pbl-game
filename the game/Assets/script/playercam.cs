86cceb599026f6d2fd96f2998cb6756e4d81b007
kaka88481917 <kaka88481917@gmail.com>
2023-10-28 16:23:53 +0700

    cam

--- the game/Assets/script/playercam.cs
+++ the game/Assets/script/playercam.cs
@@ -9,11 +9,22 @@ public class playercam : MonoBehaviour
     public Transform playerbody;
     float xrotation = 0f;
 
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     // Update is called once per frame
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mousesens * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mousesens * Time.deltaTime;
+        xrotation -= mouseY;
+        xrotation = Mathf.Clamp(xrotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xrotation, 0f, 0f);
+
 
         playerbody.Rotate(Vector3.up * mouseX);
 

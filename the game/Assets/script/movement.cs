cf7a3d5633eb5eca070f6198cd1a935f90fd1e17
kaka88481917 <kaka88481917@gmail.com>
2023-10-31 18:26:21 +0700

    gravity

--- the game/Assets/script/movement.cs
+++ the game/Assets/script/movement.cs
@@ -6,11 +6,13 @@ public class movement : MonoBehaviour
 {
     public float playerspeed = 10f;
     public CharacterController controller;
+    public float grav = -10f;
+    Vector3 velo;
 
     void Start()
     {
         
-    }
+    }   
 
     // Update is called once per frame
     void Update()
@@ -19,9 +21,15 @@ public class movement : MonoBehaviour
         float zpos = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * xpos + transform.forward * zpos;
+        
+        
 
         controller.Move(move * playerspeed * Time.deltaTime);
-
         
+        velo.y += grav * Time.deltaTime;
+
+        controller.Move(velo * Time.deltaTime);
+
+
     }
 }
